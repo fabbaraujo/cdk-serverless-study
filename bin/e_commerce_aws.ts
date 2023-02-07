349a7350f9dede26f6c7accfbcb6985936987c19
Fabrício Araújo <fabaraujo23@gmail.com>
2023-02-07 11:52:37 -0300

    feat: add app config

--- bin/e_commerce_aws.ts
+++ bin/e_commerce_aws.ts
@@ -1,5 +1,30 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import { ProductsAppStack } from '../lib/productsApp-stack'
+import { ECommerceApiStack } from '../lib/ecommerceApi-stack'
 
-const app = new cdk.App();
+const app = new cdk.App()
+
+const env: cdk.Environment = {
+  account: "585170566817",
+  region: "us-east-1"
+}
+
+const tags = {
+  cost: "ECommerce",
+  team: "Dev"
+}
+
+const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
+  tags: tags,
+  env: env
+})
+
+const eCommerceApiStack = new ECommerceApiStack(app, "ECommerceApi", {
+  productsFetchHandler: productsAppStack.productsFetchHandler,
+  tags: tags,
+  env: env
+})
+
+eCommerceApiStack.addDependency(productsAppStack)
